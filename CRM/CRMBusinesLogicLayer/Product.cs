af0daa352c2b05596de22d9d2dbc737367a21afa
andriuskin111 <andriuskin@gmail.com>
2020-01-29 01:11:53 +0200

    pridetos klases product, order

---
+++ CRM/CRMBusinesLogicLayer/Product.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CRMBusinesLogicLayer
+{
+    public class Product
+    {
+        public int ProductId { get; private set; }
+        public string ProductName { get; set; }
+        public string ProductDescription { get; set; }
+        public decimal ProductCurrentPrice { get; set; }
+
+        public Product()
+        {
+                
+        }
+
+        public Product(int productId)
+        {
+            ProductId = productId;   
+        }
+
+        public bool Validate()
+        {
+            bool isValid = true;
+
+            if (string.IsNullOrEmpty(ProductName)) isValid = false;
+            if (string.IsNullOrEmpty(ProductDescription)) isValid = false;
+            if (ProductCurrentPrice < 0) isValid = false;
+
+            return isValid;
+        }
+    }
+}
